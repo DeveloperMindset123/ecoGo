72913ce126eb931543d416f18d4f617696ccb036
garvsl <garvsehgal7@gmail.com>
2024-02-04 08:57:11 -0500

    fix header

--- App/components/CurrentLocation/CurrentLocation.tsx
+++ App/components/CurrentLocation/CurrentLocation.tsx
@@ -1,36 +1,38 @@
-import { OpenAQResult, stationName } from '@shootismoke/dataproviders';
-import React from 'react';
-import { StyleSheet, Text, TextProps } from 'react-native';
+import { OpenAQResult, stationName } from "@shootismoke/dataproviders";
+import React from "react";
+import { StyleSheet, Text, TextProps } from "react-native";
 
-import { t } from '../../localization';
-import { Location } from '../../stores/util/fetchGpsPosition';
-import * as theme from '../../util/theme';
+import { t } from "../../localization";
+import { Location } from "../../stores/util/fetchGpsPosition";
+import * as theme from "../../util/theme";
 
-const UNKNOWN_STATION = t('current_location_unknown_station');
+const UNKNOWN_STATION = t("current_location_unknown_station");
 
 interface CurrentLocationProps extends TextProps {
-	measurement: OpenAQResult;
-	currentLocation: Location;
+  measurement: OpenAQResult;
+  currentLocation: Location;
 }
 
 const styles = StyleSheet.create({
-	title: {
-		...theme.title,
-	},
+  title: {
+    ...theme.title,
+  },
 });
 
 export function CurrentLocation(
-	props: CurrentLocationProps
+  props: CurrentLocationProps
 ): React.ReactElement {
-	const { currentLocation, measurement, style, ...rest } = props;
+  const { currentLocation, measurement, style, ...rest } = props;
 
-	return (
-		<Text style={[styles.title, style]} {...rest}>
-			{(
-				currentLocation.name ||
-				stationName(measurement) ||
-				UNKNOWN_STATION
-			).toUpperCase()}
-		</Text>
-	);
+  return (
+    <Text style={[styles.title, style]} {...rest}>
+      {/* {(
+				// currentLocation.name ||
+				// stationName(measurement) ||
+				// UNKNOWN_STATION
+				
+			).toUpperCase()} */}
+      Columbia Uni | NY,US
+    </Text>
+  );
 }
